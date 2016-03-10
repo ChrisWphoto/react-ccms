f0de594ca05424351b491b58159b118eae8da381
Chris <walter.chris@gmail.com>
2016-03-09 21:19:06 -0600

    added navbar, about to add react bootstrap

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -2,67 +2,8 @@ import React from "react";
 import Griddle from 'griddle-react';
 import styles from "./style.css";
 import helpers from '../../utils/helpers';
-var fakeData =  [
-  {
-    "id": 0,
-    "name": "Mayer Leonard",
-    "city": "Kapowsin",
-    "state": "Hawaii",
-    "country": "United Kingdom",
-    "company": "Ovolo",
-    "favoriteNumber": 7
-  },
-  {
-    "id": 1,
-    "name": "Chris Leonard",
-    "city": "Kapowsin",
-    "state": "Hawaii",
-    "country": "United Kingdom",
-    "company": "Ovolo",
-    "favoriteNumber": 9
-  },
-  {
-    "id": 2,
-    "name": "Tanner",
-    "city": "Kapowsin",
-    "state": "Hawaii",
-    "country": "United Kingdom",
-    "company": "Ovolo",
-    "favoriteNumber": 11
-  },
-  {
-    "id": 10,
-    "name": "Mayer Leonard",
-    "city": "Kapowsin",
-    "state": "Hawaii",
-    "country": "United Kingdom",
-    "company": "Ovolo",
-    "favoriteNumber": 7
-  },
-  {
-    "id": 11,
-    "name": "Chris Leonard",
-    "city": "Kapowsin",
-    "state": "Hawaii",
-    "country": "United Kingdom",
-    "company": "Ovolo",
-    "favoriteNumber": 9
-  },
-  {
-    "id": 22,
-    "name": "Tanner",
-    "city": "Kapowsin",
-    "state": "Hawaii",
-    "country": "United Kingdom",
-    "company": "Ovolo",
-    "favoriteNumber": 11
-  }
-
-
-];
 
 var HomePage = React.createClass ({
-
 
   getInitialState: function(){
      return {
@@ -82,9 +23,80 @@ var HomePage = React.createClass ({
   render() {
     return (
       <div className={styles.content}>
+
+
+
+
+
+
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+                <span className="icon-bar"></span>
+              </button>
+              <a className="navbar-brand" href="#">CCMS</a>
+            </div>
+
+
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav">
+                <li className="active"><a href="#">My Dashboard <span className="sr-only">(current)</span></a></li>
+                <li className="dropdown">
+                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                  <ul className="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" className="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                    <li role="separator" className="divider"></li>
+                    <li><a href="#">One more separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+              <form className="navbar-form navbar-left" role="search">
+                <div className="form-group">
+                  <input type="text" className="form-control" placeholder="Search"></input>
+                </div>
+                <button type="submit" className="btn btn-default">Submit</button>
+              </form>
+              <ul className="nav navbar-nav navbar-right">
+                <li><a href="#">Link</a></li>
+                <li className="dropdown">
+                  <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                  <ul className="dropdown-menu">
+                    <li><a href="#">Action</a></li>
+                    <li><a href="#">Another action</a></li>
+                    <li><a href="#">Something else here</a></li>
+                    <li role="separator" className="divider"></li>
+                    <li><a href="#">Separated link</a></li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+
+
+
+
+
+
+
+
+
+
+
+
+
         <h1>Cases for {this.state.userInfo.firstName} {this.state.userInfo.LastName}</h1>
         <Griddle results={this.state.cases} tableClassName="table" showFilter={true}
-          showSettings={true} columns={["id", "dateCreated", "currentStatus", "mainType", 'subType']} />
+          showSettings={true} columns={["id", "dateCreated", "currentStatus", "mainType", 'subType']}
+          noDataMessage={"No Cases to Display. Try Refreshing the page or click Add New above."}/>
 
       </div>
     );
--- src/pages/login/page.js
+++ src/pages/login/page.js
@@ -4,7 +4,14 @@ import styles from './style.css';
 import Login from '../../common/components/Login';
 
 var LoginPage = React.createClass( {
-
+  componentDidMount: function() {
+    console.log('inside mounting');
+    let userInfo = JSON.parse(window.localStorage.getItem('user'));
+    if  (userInfo){
+      console.log('pushing to /home');
+      browserHistory.push('/home');
+    }
+  },
 
 
   render() {
@@ -12,7 +19,9 @@ var LoginPage = React.createClass( {
       <div className={styles.content}>
         <h1 className={styles.heading}>CCMS </h1>
 
+        //This is loading the Login in component which is imported above
         <Login />
+
         <p className={styles.lead}>Don't have an account? </p>
         <button type="button" onClick={this.signUp}>Sign up</button>
       </div>
